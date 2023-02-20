fcfe673bcf68cc1189a988893954d579dd429d83
Hoàng Văn Trung <hoadev69@gmail.com>
2023-02-21 00:09:32 +0700

    Update cánh hoa

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function iecompattest() {
 }
 var i, doc_width = 800,
     doc_height = 600;
-for (ns6up ? (doc_width = self.innerWidth, doc_height = self.innerHeight) : ie4up && (doc_width = iecompattest().clientWidth, doc_height = iecompattest().clientHeight), dx = new Array, xp = new Array, yp = new Array, am = new Array, stx = new Array, sty = new Array, i = 0; i < no; ++i) dx[i] = 0, xp[i] = Math.random() * (doc_width - 50), yp[i] = Math.random() * doc_height, am[i] = 20 * Math.random(), stx[i] = .02 + Math.random() / 10, sty[i] = .7 + Math.random(), (ie4up || ns6up) && document.write('<div id="dot' + i + '" style="POSITION:absolute;Z-INDEX:' + i + ';VISIBILITY:visible;TOP:15px;LEFT:15px;"><span style="font-size:21px;color:#FFFF">*</span></div>');
+for (ns6up ? (doc_width = self.innerWidth, doc_height = self.innerHeight) : ie4up && (doc_width = iecompattest().clientWidth, doc_height = iecompattest().clientHeight), dx = new Array, xp = new Array, yp = new Array, am = new Array, stx = new Array, sty = new Array, i = 0; i < no; ++i) dx[i] = 0, xp[i] = Math.random() * (doc_width - 50), yp[i] = Math.random() * doc_height, am[i] = 20 * Math.random(), stx[i] = .02 + Math.random() / 10, sty[i] = .7 + Math.random(), (ie4up || ns6up) && document.write('<div id="dot' + i + '" style="POSITION:absolute;Z-INDEX:' + i + ';VISIBILITY:visible;TOP:15px;LEFT:15px;"><span style="font-size:12px;color:#FFFF;">🌸</span></div>');
 
 function snowIE_NS6() {
     for (doc_width = ns6up ? window.innerWidth - 10 : iecompattest().clientWidth - 10, doc_height = window.innerHeight && "windowheight" == snowdistance ? window.innerHeight : ie4up && "windowheight" == snowdistance ? iecompattest().clientHeight : ie4up && !window.opera && "pageheight" == snowdistance ? iecompattest().scrollHeight : iecompattest().offsetHeight, i = 0; i < no; ++i) yp[i] += sty[i], yp[i] > doc_height - 50 && (xp[i] = Math.random() * (doc_width - am[i] - 30), yp[i] = 0, stx[i] = .02 + Math.random() / 10, sty[i] = .7 + Math.random()), dx[i] += stx[i], document.getElementById("dot" + i).style.top = yp[i] + "px", document.getElementById("dot" + i).style.left = xp[i] + am[i] * Math.sin(dx[i]) + "px";
@@ -23,3 +23,13 @@ function hidesnow() {
 function hidesnow() {
     for (window.snowtimer && clearTimeout(snowtimer), i = 0; i < no; i++) document.getElementById("dot" + i).style.visibility = "hidden"
 }(ie4up || ns6up) && (snowIE_NS6(), hidesnowtime > 0 && setTimeout("hidesnow()", 1e3 * hidesnowtime));
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
